7fd7d755e581279c8b6cfae2f799157f5c734b9a
ONIK698 <olganik240998@gmail.com>
2018-12-14 00:18:33 +0400

    Create admin panel

--- app/Http/Controllers/Admin/AccountController.php
+++ app/Http/Controllers/Admin/AccountController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 
@@ -8,6 +8,6 @@ class AccountController extends Controller
 {
 	public function index()
     {
-        return view('account.index');
+        return view('admin.index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -25,12 +25,22 @@ Route::group(['middleware'=>'guest'], function(){
 	Route::get('/login','Auth\LoginController@showLoginForm')->name('login');
 	Route::post('/login','Auth\LoginController@login');
 });
-
+//account
 Route::group(['middleware'=>'auth'], function(){
+	Route::get('/logout', function(){
+		\Auth::logout();
+		return redirect(route('login'));
+	})->name('logout');
 	Route::get('/my/account','AccountController@index')->name('account');
+
+//Admin
+	Route::group(['middleware'=>'admin'], function(){
+		Route::get('/admin','Admin\AccountController@index')->name('admin');
+	});
+
 });
 
-
+//___
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
