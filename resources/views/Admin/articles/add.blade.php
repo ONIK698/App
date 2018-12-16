ffc5d8c65b51ec09f21a725e18bb05ac1ffcc313
ONIK698 <olganik240998@gmail.com>
2018-12-16 23:35:49 +0400

    Add Articles

---
+++ resources/views/Admin/articles/add.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.admin')
+@section('content')
+<?php
+   // if(session()->has('errors')){
+   //     $errors = session()->get('errors');
+   //     foreach ($errors->all(":message") as $message) {
+   //         echo $message . "<br>";
+   //     }
+   //     dd(222);
+   // }
+?>
+    <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+        <h1>Добавить обьявление</h1>
+        <br>
+        <form method="post">
+            {!! csrf_field() !!}
+            <p>Выбор категории (ий):<br><select name="categories[]" class="form-control" multiple>
+                  @foreach($categories as $category)
+                       <option value="{{$category->id}}">{{$category->title}}</option>
+                  @endforeach
+            </select> </p>
+        <p>Название товара:<br><input type="text" name="title" class="form-control" required> </p>
+        <p>Автор:<br><input type="text" name="author" class="form-control" required> </p>
+        <p>Короткое описание:<br><textarea name="short_text" class="form-control"></textarea></p>
+        <p>Полное описание:<br><textarea name="full_text" class="form-control"></textarea></p>
+        <!-- <p>Отзывы:<br><textarea name="feedback" class="form-control"></textarea></p> -->
+        <br><br>
+        <button type="submit" class="btn btn-success" style="cursor: pointer; float: right;">Добавить</button>
+        </form>
+    </main>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -36,21 +36,22 @@ Route::group(['middleware'=>'auth'], function(){
 //Admin
 	Route::group(['middleware'=>'admin', 'prefix'=>'admin'], function(){
 		Route::get('/','Admin\AccountController@index')->name('admin');
-
+//Categories
 		Route::get('/categories','Admin\CategoriesController@index')->name('categories');
-
 		Route::get('/categories/add','Admin\CategoriesController@addCategories')->name('categories.add');
 		Route::post('/categories/add','Admin\CategoriesController@addRequestCategory');
-
 		Route::get('/categories/edit/{id}','Admin\CategoriesController@editCategory')
 		    ->where('id', '\d+')
 		    ->name('categories.edit');
-
 		Route::post('/categories/edit/{id}','Admin\CategoriesController@editRequestCategory')
 		    ->where('id', '\d+');
-
 		Route::delete('/categories/delete','Admin\CategoriesController@deleteCategories')->name('categories.delete');
-
+//Articles
+		Route::get('/articles','Admin\ArticlesController@index')->name('articles');
+		Route::get('/articles/add','Admin\ArticlesController@addArticle')->name('articles.add');
+		Route::post('/articles/add','Admin\ArticlesController@addRequestArticle');
+		Route::get('/articles/edit/{id}','Admin\ArticlesController@editArticle')->where('id', '\d+')->name('articles.edit');
+		Route::delete('/articles/delete','Admin\ArticlesController@deleteArticle')->name('articles.delete');
 
 	});
 
